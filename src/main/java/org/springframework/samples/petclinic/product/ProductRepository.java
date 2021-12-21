675e2f6e04a22e63e6d15d5c79268323fa75ff5f
Pablo Álvarez <80222536+Pabalvcar@users.noreply.github.com>
2021-12-21 19:25:26 +0100

    Commit del simulacro

--- src/main/java/org/springframework/samples/petclinic/product/ProductRepository.java
+++ src/main/java/org/springframework/samples/petclinic/product/ProductRepository.java
@@ -3,14 +3,26 @@ package org.springframework.samples.petclinic.product;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 
 
-public interface ProductRepository {
+public interface ProductRepository extends CrudRepository<Product, Integer>{
     List<Product> findAll();
+    
+    @Query("SELECT p FROM ProductType p")
     List<ProductType> findAllProductTypes();
+    
     Optional<Product> findById(int id);
+    
+    @Query("SELECT p FROM ProductType p WHERE p.name=:name")
+    ProductType findByProductTypeName(@Param("name") String name);
+    
+    @Query("SELECT p FROM Product p WHERE p.price <:price")
+    List<Product> findByPriceLessThan(@Param("price") Double price);
+    
     Product findByName(String name);
     Product save(Product p);
 }
--- src/main/java/org/springframework/samples/petclinic/product/ProductService.java
+++ src/main/java/org/springframework/samples/petclinic/product/ProductService.java
@@ -2,17 +2,29 @@ package org.springframework.samples.petclinic.product;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProductService {
+	
+	@Autowired
+	private ProductRepository productRepository;
+	
+	public ProductService (ProductRepository productRepository) {
+		this.productRepository = productRepository;
+	}
+	
     public List<Product> getAllProducts(){
-        return null;
+        return productRepository.findAll();
     }
 
     public List<Product> getProductsCheaperThan(double price) {
-        return null;
+        return productRepository.findByPriceLessThan(price);
     }
 
     public ProductType getProductType(String typeName) {
-        return null;
+        return productRepository.findByProductTypeName(typeName);
     }
 
     public Product save(Product p){
